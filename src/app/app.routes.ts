2e0bcdf41f2be94d858f67bdcec27f1090fd5cf6
Diego Alexander Higuita Jaramillo <dahiguita@tcc.com.co>
2024-06-01 21:57:11 -0500

    Detalles de cada producto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,30 @@
 import { Routes } from '@angular/router';
 
 import { ListComponent } from '@products/pages/list/list.component';
+import { ProductDetailComponent } from '@products/pages/product-detail/product-detail.component';
+import { LayoutComponent } from '@shared/components/layout/layout.component';
 import { AboutComponent } from '@info/pages/about/about.component';
 import { NotFoundComponent } from '@info/pages/not-found/not-found.component';
-import {HeaderComponent} from '@shared/components/header/header.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: ListComponent
-    },
-    {
-        path: 'about',
-        component: AboutComponent
-    },
-    {
-        path: 'header',
-        component: HeaderComponent
-    },
+        component: LayoutComponent,
+        children: [
+            {
+                path: '',
+                component: ListComponent
+            },
+            {
+                path: 'about',
+                component: AboutComponent
+            },
+            {
+                path: 'product/:id',
+                component: ProductDetailComponent
+            }
+        ]
+    },  
     {
         path: '**',
         component: NotFoundComponent
