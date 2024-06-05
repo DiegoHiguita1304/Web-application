2a0ec27b9cdb03414b6d8f100726713238187526
Diego Alexander Higuita Jaramillo <dahiguita@tcc.com.co>
2024-06-04 22:24:15 -0500

    Url Params

--- src/app/domains/shared/services/product.service.ts
+++ src/app/domains/shared/services/product.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Product } from '../models/product.models';
+import { url } from 'inspector';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,12 @@ export class ProductService {
 
   constructor() { }
 
-  getProducts() {
-    return this.http.get<Product[]>(`https://api.escuelajs.co/api/v1/products`);
+  getProducts(category_id?: string) {
+    const url = new URL(`https://api.escuelajs.co/api/v1/products`);
+    if (category_id) {
+      url.searchParams.set('categoryId', category_id)
+    }
+    return this.http.get<Product[]>(url.toString());
   }
 
   getOne(id: string) {
